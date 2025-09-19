0ddee49edab0bbc5e71b1ce6aa1ee9de6f4bb545
CHAITANYA KAMBLE <sondo0963@gmail.com>
2025-09-19 08:42:39 +0000

    on netlify

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  "projectId": "studio-1052878063-3a28a",
-  "appId": "1:813623161084:web:700c033da623462814e779",
-  "apiKey": "AIzaSyAbuwT8eOJ4MJUQ6iEZQyDUrIHyBG4Qs1w",
-  "authDomain": "studio-1052878063-3a28a.firebaseapp.com",
-  "storageBucket": "studio-1052878063-3a28a.appspot.com",
-  "messagingSenderId": "813623161084",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
